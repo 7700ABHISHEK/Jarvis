2871f036c29fc514d348adb8d1cdc9c924e26ea1
Abhishek dubey <7700.Abhishekdubey02@gmai.com>
2025-05-21 22:12:04 +0530

    added some commands

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,9 +30,37 @@ recognition.onresult = function(e){
     let currentIdx = e.resultIndex;
 
     let transcript = e.results[currentIdx][0].transcript;
+    transcript = transcript.toLowerCase();
     console.log(transcript);
 
-    speakOut(transcript);
+    if(transcript.includes("hare krishna")){
+        speakOut("Jay Srila Prabhupada");
+    }
+
+    if(transcript.includes("open youtube")){
+        speakOut("opening youtube");
+        window.open("https://www.youtube.com/");
+    }
+
+    if(transcript.includes("open google")){
+        speakOut("opening google");
+        window.open("https://www.google.com/");
+    }
+
+    if(transcript.includes("open firebase")){
+        speakOut("opening firbase");
+        window.open("https://console.firebase.google.com/");
+    }
+
+    if(transcript.includes("search for")){
+        speakOut("Here's the result");
+        let input = transcript.toLowerCase().replace("search for","").trim();
+        // input.splice(0, 11)
+        input = input.split(" ").join("+");
+        console.log(input);
+        window.open(`https://www.google.com/search?q=${input}`)
+    }
+
 }
 
 // vr continuous
@@ -64,7 +92,7 @@ function speakOut(message){
 }
 
 speakBtn.addEventListener("click", ()=>{
-    speakOut("...Hare Krsna, Abhishek it's me jarvis");
+    speakOut("Kaa hoo Abhishek babua ham jarvis bola thai");
 })
 
 window.onload = function(){
